3c105d90789bef543ab5e3226e3db6fcbadf163c
Jarin <khanjannath2020@gmail.com>
2023-05-19 14:01:35 +0600

    get commit

--- index.js
+++ index.js
@@ -24,12 +24,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const db = client.db("softToys");
+const toysCollection = db.collection("toys");
+
+// console.log("database connected")
+
+app.post("/postToys", async (req, res) => {
+    const body = req.body;
+    const result = await toysCollection.insertOne(body);
+    console.log(result);
+    res.send(result);
+});
+
+app.get("/allToys/:text", async(req, res) =>{
+    console.log(req.params.text);
+    if(req.params.text == "animal" || req.params.text == "character" || req.params.text == "fantasy"){
+        const result = await toysCollection.find({ category: req.params.text }).toArray();
+        console.log(result)
+      return  res.send(result);
+    }
+     const result = await toysCollection.find({}).toArray();
+     res.send(result);
+});
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
